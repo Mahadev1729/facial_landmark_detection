991be6cb51cad7d6a99f1682ffffff19390c40f7
Mahadev1729 <devcampus17@gmail.com>
2025-03-22 18:33:45 +0530

    remove unwanted code

--- src/App.js
+++ src/App.js
@@ -6,27 +6,19 @@ import './App.css';
 import React,{useRef} from 'react'
 import * as tf  from "@tensorflow/tfjs";
 import * as facemesh from "@tensorflow-models/facemesh";
-import Webcam
- from 'react-webcam';
+import Webcam from 'react-webcam';
 
 
 function App() {
+   // creating webcam refernce
+  const webcamref=useRef(null);
+  const canvasref = useRef(null);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
