a9f8cd8a440a21c52c948f982d01f21245a87e8f
Mahadev1729 <devcampus17@gmail.com>
2025-03-24 01:28:42 +0530

    draw the points

--- src/utlities.js
+++ src/utlities.js
@@ -1,5 +1,3 @@
-// Triangular matrics
-
 //  Triangulation sets of three
 export const TRIANGULATION = [
     127,
@@ -2643,23 +2641,23 @@ export const TRIANGULATION = [
     448,
     255,
 ];
+export const drawMesh = (predictions, ctx) => {
+    if (predictions.length > 0) {
+        predictions.forEach((prediction) => {
+            const keypoints = prediction.scaledMesh;
 
-// draw triangle
-// draw the points
+            
 
-export const drawMesh=(predictions,ctx)=>{
-    if(predictions.length>0){
-        predictions.forEach(prediction=>{
-            const keypoints=prediction.scaledMesh;
-            for(let i=0; i<keypoints.length; i++){
-                const x=keypoints[i][0];
-                const y=keypoints[i][1];
+            // Draw Dots
+            for (let i = 0; i < keypoints.length; i++) {
+                const x = keypoints[i][0];
+                const y = keypoints[i][1];
+
                 ctx.beginPath();
-                ctx.arc(x, y, 0, 3 * Math.PI);
-                ctx.fillStyle="aqua";
+                ctx.arc(x, y, 1 /* radius */, 0, 3 * Math.PI);
+                ctx.fillStyle = "aqua";
                 ctx.fill();
-
             }
         });
-    };
+    }
 };
