517ac02e5369bb314b036bb07e1916fda26e84bf
Mahadev1729 <devcampus17@gmail.com>
2025-03-24 00:39:13 +0530

    create detect function  to add  // get  video properties        // set video width        // set canvas width        // make detection        // get canvas context for drawing

--- src/App.js
+++ src/App.js
@@ -3,46 +3,63 @@
 // import logo from './logo.svg';
 import './App.css';
 
-import React,{useRef} from 'react'
-import * as tf  from "@tensorflow/tfjs";
+import React, { useRef } from 'react'
+import * as tf from "@tensorflow/tfjs";
 import * as facemesh from "@tensorflow-models/facemesh";
 import Webcam from 'react-webcam';
 
 
 function App() {
-   // creating webcam refernce
-  const webcamref=useRef(null);
+  // creating webcam refernce
+  const webcamref = useRef(null);
   const canvasref = useRef(null);
+
+  const runFacemesh = async () => {
+    const net = await facemesh.load({
+      inputResolution: { width: 640, height: 480 }, scale: 0.0
+    })
+  }
+  // create detect function
+  const detect = async (net) => {
+    if (typeof webcamref.current !== "undefined" && webcamref.current !== null && webcamref.current.video.readState === 4){
+       // get  video properties
+       // set video width
+       // set canvas width
+       // make detection
+       // get canvas context for drawing
+       
+      }
+};
   return (
     <div className="App">
       <Webcam ref={webcamref} style={
         {
-         position:"absolute",
-         marginLeft:"auto",
-         marginRight:"auto",
-         left:0,
-         right:0,
-         textAlign:"center",
-         width:640,
-         zIndex:9,
-         height:480
-         
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          width: 640,
+          zIndex: 9,
+          height: 480
+
         }
-      }/>
-        <canvas ref={canvasref} style={
-          {
-            position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
-            left: 0,
-            right: 0,
-            textAlign: "center",
-            width: 640,
-            zIndex: 9,
-            height: 480
-          }
+      } />
+      <canvas ref={canvasref} style={
+        {
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          width: 640,
+          zIndex: 9,
+          height: 480
         }
-        />
+      }
+      />
     </div>
   );
 }
