823ff426ed38c986c5599b9c755365456a6609fd
Mahadev1729 <devcampus17@gmail.com>
2025-03-24 00:26:08 +0530

    set up web camera

--- src/App.js
+++ src/App.js
@@ -29,8 +29,20 @@ function App() {
          
         }
       }/>
-
-
+        <canvas ref={canvasref} style={
+          {
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            width: 640,
+            zIndex: 9,
+            height: 480
+          }
+        }
+        />
     </div>
   );
 }
