db3ebaec8fbb9b7e5948608fe4e96f79e7a2ac07
Mahadev1729 <devcampus17@gmail.com>
2025-03-24 18:58:18 +0530

    drawing points not working

--- src/utlities.js
+++ src/utlities.js
@@ -2643,19 +2643,19 @@ export const TRIANGULATION = [
 ];
 export const drawMesh = (predictions, ctx) => {
     if (predictions.length > 0) {
-        predictions.forEach((prediction) => {
+        predictions.forEach(prediction => {
             const keypoints = prediction.scaledMesh;
 
             
 
-            // Draw Dots
+            // Draw Dot
             for (let i = 0; i < keypoints.length; i++) {
                 const x = keypoints[i][0];
                 const y = keypoints[i][1];
 
                 ctx.beginPath();
                 ctx.arc(x, y, 1 /* radius */, 0, 3 * Math.PI);
-                ctx.fillStyle = "red";
+                ctx.fillStyle = "aqua";
                 ctx.fill();
             }
         });
